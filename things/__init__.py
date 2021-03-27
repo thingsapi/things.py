5df6d510e40083ff8a0b141c73c027b886edfad2
Alexander Willner <alex@willner.ws>
2021-03-27 13:24:25 +0100

    cleanup

--- things/__init__.py
+++ things/__init__.py
@@ -1,3 +1,6 @@
+"""A simple Python 3 library to read your Things app data."""
+
+
 from things.api import (areas, canceled, completed,  # noqa
                         inbox, projects, tags, tasks, today)  # noqa
 from things.database import Database  # noqa
