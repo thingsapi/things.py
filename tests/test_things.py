18cb914376a1bc75dd116abb24c7b5cb1d7721db
Alexander Willner <alex@willner.ws>
2021-03-28 21:41:23 +0200

    new testing database

--- tests/test_things.py
+++ tests/test_things.py
@@ -7,32 +7,42 @@ import unittest
 from things import api
 
 
-DEMO_FILEPATH = "tests/demo.sqlite3"
+DEMO_FILEPATH = "tests/main.sqlite"
 
 
 class ThingsCase(unittest.TestCase):
     """Class documentation goes here."""
 
+    def test_inbox(self):
+        """Test inbox."""
+        tasks = api.inbox(filepath=DEMO_FILEPATH)
+        self.assertEqual(1, len(tasks))
+
+    def test_today(self):
+        """Test today."""
+        tasks = api.today(filepath=DEMO_FILEPATH)
+        self.assertEqual(3, len(tasks)) # todo: should be 1
+
     def test_tasks(self):
         """Test all tasks."""
         tasks = api.tasks(start="Anytime", filepath=DEMO_FILEPATH)
-        self.assertEqual(37, len(tasks))
+        self.assertEqual(5, len(tasks))
         tasks = api.tasks(start="Anytime", status="completed", filepath=DEMO_FILEPATH)
-        self.assertEqual(1, len(tasks))
+        self.assertEqual(6, len(tasks))
         tasks = api.tasks(status="completed", filepath=DEMO_FILEPATH)
-        self.assertEqual(3, len(tasks))
+        self.assertEqual(10, len(tasks))
         with self.assertRaises(ValueError):
             api.tasks(status="wrong_value", filepath=DEMO_FILEPATH)
 
     def test_tags(self):
         """Test all tags."""
         tags = api.tags(filepath=DEMO_FILEPATH)
-        self.assertEqual(14, len(tags))
+        self.assertEqual(5, len(tags))
 
     def test_projects(self):
         """Test all projects."""
         projects = api.projects(filepath=DEMO_FILEPATH)
-        self.assertEqual(7, len(projects))
+        self.assertEqual(2, len(projects))
 
     def test_areas(self):
         """Test all test_areas."""
